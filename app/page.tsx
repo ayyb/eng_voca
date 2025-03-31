ab24abcf6eef9f048a1496524d0d35f4f96e0e80
이명섭 <abbbc@mz.co.kr>
2025-03-31 19:30:01 +0900

    feat: 메인 배경화면 추가

--- app/page.tsx
+++ app/page.tsx
@@ -44,15 +44,15 @@ const HomePage = () => {
     <div className="min-h-screen bg-white">
       <div
         className="w-full max-w-md mx-auto h-screen flex flex-col p-8 
-                      border border-gray-200 rounded-2xl shadow-lg"
+                      border border-gray-200 rounded-2xl shadow-lg bg-main"
       >
         {/* 상단 텍스트 영역 */}
         <div className="flex-1 flex flex-col justify-center">
           <div className="space-y-6">
-            <p className="font-bold text-5xl sm:text-7xl text-black">Daily</p>
-            <p className="font-bold text-5xl sm:text-7xl text-black">English</p>
-            <p className="font-bold text-5xl sm:text-7xl text-black">Voca</p>
-            <p className="text-lg sm:text-xl text-black mt-8">
+            <p className="font-bold text-5xl sm:text-7xl text-white">Daily</p>
+            <p className="font-bold text-5xl sm:text-7xl text-white">English</p>
+            <p className="font-bold text-5xl sm:text-7xl text-white">Voca</p>
+            <p className="text-lg sm:text-xl text-white mt-8">
               매일매일 외우는 영어단어장
             </p>
           </div>
